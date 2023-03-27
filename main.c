f77144fad4c9e2cb10bebc154105e75b1e6dd240
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-27 04:03:40 -0700

    Added main

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	put_char('A');
+	put_char('\n');
+
+	return (0);
+}
