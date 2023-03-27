1969602fb227de862581903b68f4aab527f47612
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-27 05:09:39 -0700

    base

---
+++ convert_decimal_to_base_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ *
+ * @decimal: character
+ * @len: int
+ * @base: int
+ * @str: string
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+char *convert_decimal_to_base_string(int decimal, int len, int base, char *str)
+{
+	int remainder;
+
+	if (decimal > 0)
+	{
+		remainder = decimal % base;
+
+		if (remainder >= 10)
+		{
+			str[len - 1] = remainder + 87;
+		}
+		else
+		{
+			str[len - 1] = remainder + 48;
+		}
+
+		convert_decimal_to_base_string(decimal / base, len - 1, base, str);
+	}
+
+	return (str);
+}
--- main.h
+++ main.h
@@ -5,10 +5,9 @@ int _printf(const char *format, ...);
 void put_char(char character);
 int get_base_length(int decimal, int base);
 int get_string_length(char *string);
-char *convert_decimal_to_binary_string(int decimal, int length);
-char *convert_decimal_to_octal_string(int decimal, int length);
-char *convert_deciaml_to_hexa_string(int decimal, int length);
+char *convert_decimal_to_base_string(int decimal, int len, int base, char *str);
 char *convert_address_to_string(void *ptr);
+void add_null_terminator(char *string, int length);
 char *reverse_string(char *string);
 char *rotate13_string(char *string);
 
