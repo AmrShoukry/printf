dc322e016f3629c4fbfa08ff757b2e921fd8ab99
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-28 03:17:40 -0700

    f

--- _printf.c
+++ _printf.c
@@ -49,27 +49,33 @@ int percentage(int *percentage_ptr, char c)
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i = 0;
-	int percentage_mode = 0;
-	int counter = 0;
+	int i = 0, percentage_mode = 0, counter = 0, arg_int, length;
 	int *percentage_pointer = &percentage_mode;
-	int arg_int, length;
-	char *temp;
-	char *string;
+	char *temp, *string;
+	char char_argument;
 
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
-		{
 			counter += percentage(percentage_pointer, format[i]);
-		}
 		else if ((format[i] == 'd' || format[i] == 'i') && *percentage_pointer == 1)
 		{
 			arg_int = va_arg(ap, int);
 			length = get_base_length(arg_int, 10);
 			temp = (char *) malloc(sizeof(char) * (length + 1));
 			string = convert_decimal_to_base_string(arg_int, length, 10, temp);
+			counter += _printf(string);
+		}
+		else if (format[i] == 'c' && *percentage_pointer == 1)
+		{
+			char_argument = va_arg(ap, int);
+			put_char(char_argument);
+			counter++;
+		}
+		else if (format[i] == 's' && *percentage_pointer == 1)
+		{
+			string = va_arg(ap, char *);
 			counter += _printf(string);
 		}
 		else
@@ -79,8 +85,6 @@ int _printf(const char *format, ...)
 		}
 		i++;
 	}
-
 	va_end(ap);
-
 	return (counter);
 }
